5de1219995af4af468c8ed914ab9a56c6486a346
Joonas <e2001398@edu.vamk.fi>
2022-04-23 17:49:59 +0300

    ID.ico ja title

--- client/src/pages/Addnew.jsx
+++ client/src/pages/Addnew.jsx
@@ -2,11 +2,12 @@ import { useState } from "react"
 import AddDriverLisence from "../components/addNewCards/AddDriverlicense"
 import AddPassport from "../components/addNewCards/AddPassport"
 import AddKela from "../components/addNewCards/AddKela"
+import Placehodler from "../components/addNewCards/Placeholder";
 
 
 export default function Addnew(){
 
-    const [state, setState] = useState("Ajokortti");
+    const [state, setState] = useState("Default");
 
     const changeState = (newState) => {
         setState(newState)
@@ -15,11 +16,12 @@ export default function Addnew(){
     return (
         <div className="addNew">
             <select className="addNewSelect" onChange={(event) => changeState(event.target.value)}>
-                <option default hidden>Valitse uusi ID</option>
+                <option default hidden value="Default">Valitse uusi ID</option>
                 <option value="Passi">Passi</option>
                 <option value="Ajokortti">Ajokortti</option>
                 <option value="Kelakortti">Kelakortti</option>
             </select>
+            {state === "Default" && <Placehodler />}
             {state === "Passi" && <AddPassport/>}
             {state === "Ajokortti" && <AddDriverLisence />}
             {state === "Kelakortti" && <AddKela />}
