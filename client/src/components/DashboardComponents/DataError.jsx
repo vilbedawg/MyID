0249d8ed51458a7487a8dc6918d20fe19b2482a3
Vilho Luoma <vilho.luoma@gmail.com>
2022-04-26 13:57:01 +0300

    some final css fixing

--- client/src/components/DashboardComponents/DataError.jsx
+++ client/src/components/DashboardComponents/DataError.jsx
@@ -1,6 +1,10 @@
 import { DeclinedIcon } from "../../components/icons/Declined-icon";
-
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 export default function DataError (){
+    useEffect(() => {
+        toast.warn('Tiedostosi on viallinen');
+    }, [])
     return(
         <div className="dashboardRight" style={{flexDirection: 'column', justifyContent: 'flex-start'}}>
             <DeclinedIcon inValid={true}/>
