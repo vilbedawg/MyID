bfaf792ae311746635ed2089b719bff95b263ead
Vilho Luoma <vilho.luoma@gmail.com>
2022-04-28 13:15:14 +0300

    cors config

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = 'http://localhost:3000';
+const baseURL = 'http://localhost:3000' ||  'https://oma-id.herokuapp.com';
 
 export default axios.create({
     baseURL: baseURL
